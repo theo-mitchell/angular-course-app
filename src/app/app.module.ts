5450989609e7c4873b93f1df33d2aef52e1b5900
theom95 <theodore.mitchell95@gmail.com>
2021-12-05 09:50:05 -0600

    Finished section 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './components/shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './components/recipe-book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './components/recipe-book/recipe-item/recipe-item.component';
+import { RecipeDetailComponent} from "./components/recipe-book/recipe-detail/recipe-detail.component";
+import { RecipesComponent } from './components/recipe-book/recipes/recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/shopping-list/shopping-list/shopping-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Ingredient} from "../../shared/ingredient.model";
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+
+  ingredients: Ingredient[] = [
+    new Ingredient("TestIngridient", 3),
+    new Ingredient("AssFruit", 10),
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
